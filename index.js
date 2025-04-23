c058af2a0c44e19317b75c5371dc68ec35f85ceb
Adrian Gidaro <adriangidaro@gmail.com>
2025-04-23 21:07:02 +1000

    Created seed.js; refactored mongoose connection into db.js; added seed script to package.json

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function print(value) {
 
 // const express = require('express') // CommonJS modules - same thing as import
 import express from 'express' // ES6 modules - requires "type": "module" to be added to package.json
-import mongoose from 'mongoose'
 // Internal import can be named anything as default exports are anonymous by default
 import post_routes from './routes/post_routes.js'
+import db from './db.js'
 
 const app = express()
 const port = 3000
@@ -21,7 +21,5 @@ app.use(post_routes)
 
 app.listen(port, async () => { // Starts up server by calling listen method
     print(`Example app listening on port ${port}.`) // Callback is called when the server is running
-    // Connect to MongoDB
-    await mongoose.connect('mongodb://127.0.0.1:27017/nodeintro_db') // DB connection string
-    print(mongoose.connection.readyState == 1 ? 'Mongoose connected.' : 'Mongoose failed to connect!') // Ternary operator
+    db.connect()
 })
