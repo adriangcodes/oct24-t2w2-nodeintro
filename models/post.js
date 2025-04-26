b3f429109c801cfde0b53cde77a320e1738f8d8a
Adrian Gidaro <adriangidaro@gmail.com>
2025-04-26 13:09:17 +1000

    Completed CRUD routes

--- models/post.js
+++ models/post.js
@@ -3,9 +3,9 @@ import { model } from 'mongoose'
 // Flask: ORM (Object-Relational Mapper - SQLAlchemy) - defined models
 // Express: ODM (Object-Document Mapper - Mongoose)
 const Post = model('Post', {
-    title: String,
-    body: String,
-    isPublished: Boolean
+    title: { type: String, required: [true, 'Title is required.'] },
+    body: { type: String, required: [true, 'Body is required.'] },
+    isPublished: { type: Boolean, default: false }
 })
 
 export default Post
